8ce7bb0783e2ed6ebabceb8fd3a3585d8888f87f
YoungChulShin <go1323@gmail.com>
2021-03-04 05:19:04 +0900

    의존성을 직접 생성하는 코드 추가

---
+++ ch2_di/src/main/java/study/spring/ch2_di/Ch2DiApplication.java
@@ -0,0 +1,13 @@
+package study.spring.ch2_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch2DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch2DiApplication.class, args);
+    }
+
+}
---
+++ ch2_di/src/test/java/study/spring/ch2_di/Ch2DiApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.ch2_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch2DiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
