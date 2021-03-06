417ec3a9571b24860d688d77aaefd856ff3a8a9d
YoungChulShin <go1323@gmail.com>
2021-03-07 06:57:20 +0900

    modify printf code error

--- ch2_di/src/main/java/study/spring/ch2_di/MemberPrinter.java
+++ ch2_di/src/main/java/study/spring/ch2_di/MemberPrinter.java
@@ -1,13 +1,17 @@
 package study.spring.ch2_di;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
 public class MemberPrinter {
 
     private DateTimeFormatter dateTimeFormatter;
+
+    public MemberPrinter() {
+        dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+    }
 
     public void print(Member member) {
         if (dateTimeFormatter == null) {
@@ -16,18 +20,23 @@ public class MemberPrinter {
                     member.getId(), member.getEmail(), member.getName(), member.getRegisterDateTime());
         } else {
             System.out.printf(
-                    "회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%tF\n",
+                    "회원 정보: 아이디=%d, 이메일=%s, 이름=%s, 등록일=%s\n",
                     member.getId(), member.getEmail(), member.getName(),
                     dateTimeFormatter.format(member.getRegisterDateTime()));
         }
     }
 
     @Autowired
-    public void setDateTimeFormatter(Optional<DateTimeFormatter> dateTimeFormatter) {
-        if (dateTimeFormatter.isPresent()) {
-            this.dateTimeFormatter = dateTimeFormatter.get();
-        } else {
-            this.dateTimeFormatter = null;
-        }
+    public void setDateTimeFormatter(@Nullable  DateTimeFormatter dateTimeFormatter) {
+        this.dateTimeFormatter = dateTimeFormatter;
     }
+
+//    @Autowired
+//    public void setDateTimeFormatter(Optional<DateTimeFormatter> dateTimeFormatter) {
+//        if (dateTimeFormatter.isPresent()) {
+//            this.dateTimeFormatter = dateTimeFormatter.get();
+//        } else {
+//            this.dateTimeFormatter = null;
+//        }
+//    }
 }
