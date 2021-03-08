1e1387173aeb61f99f17514d2e1100d4dc1ed7d1
YoungChulShin <go1323@gmail.com>
2021-03-09 05:38:14 +0900

    Implement calculator class

---
+++ ch7_aop/src/main/java/study/spring/ch7_aop/ImpeCalculator.java
@@ -0,0 +1,13 @@
+package study.spring.ch7_aop;
+
+public class ImpeCalculator implements Calculator {
+    @Override
+    public long factorial(long num) {
+        long result = 1;
+        for (long i = 1; i <= num; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+}
