dee374558bf4784f04794149273fdf4ea37f5671
YoungChulShin <go1323@gmail.com>
2021-03-09 05:57:11 +0900

    Implement proxy class for common function

--- ch7_aop/src/main/java/study/spring/ch7_aop/Ch7AopApplication.java
+++ ch7_aop/src/main/java/study/spring/ch7_aop/Ch7AopApplication.java
@@ -3,11 +3,17 @@ package study.spring.ch7_aop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Ch7AopApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Ch7AopApplication.class, args);
+//        SpringApplication.run(Ch7AopApplication.class, args);
+
+        ExecTimeCalculator execTimeCalculator1 = new ExecTimeCalculator(new ImpeCalculator());
+        System.out.println(execTimeCalculator1.factorial(20));
+
+        ExecTimeCalculator execTimeCalculator2 = new ExecTimeCalculator(new RecCalculator());
+        System.out.println(execTimeCalculator2.factorial(20));
     }
 
 }
---
+++ ch7_aop/src/main/java/study/spring/ch7_aop/ExecTimeCalculator.java
@@ -0,0 +1,23 @@
+package study.spring.ch7_aop;
+
+public class ExecTimeCalculator implements Calculator {
+
+    private Calculator delegate;
+
+    public ExecTimeCalculator(Calculator delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public long factorial(long num) {
+        long start = System.nanoTime();
+        long result = delegate.factorial(num);
+        long end = System.nanoTime();
+        System.out.printf("%s.factory(%d) 실행 시간 = %d\n",
+                delegate.getClass().getSimpleName(),
+                num,
+                (end - start));
+
+        return result;
+    }
+}
