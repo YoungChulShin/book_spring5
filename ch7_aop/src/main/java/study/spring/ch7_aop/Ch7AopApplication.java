ca669de090bf5569efbfa6270c833c9f673dd4c0
YoungChulShin <go1323@gmail.com>
2021-03-09 05:33:11 +0900

    Project scaffold

---
+++ ch7_aop/src/main/java/study/spring/ch7_aop/Ch7AopApplication.java
@@ -0,0 +1,13 @@
+package study.spring.ch7_aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch7AopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch7AopApplication.class, args);
+    }
+
+}
---
+++ ch7_aop/src/test/java/study/spring/ch7_aop/Ch7AopApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.ch7_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch7AopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
